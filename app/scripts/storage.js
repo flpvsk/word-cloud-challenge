bc7527e9cc3e918896edc061e7e64242b33c4123
filipovsky <filipovskii.off@gmail.com>
2014-07-02 18:07:50 +0400

    Release1.

---
+++ app/scripts/storage.js
@@ -0,0 +1,32 @@
+var Promise = require('es6-promise').Promise;
+
+/**
+ * Fetch a list of topics.
+ * Returns an object.
+ *
+ * Example of result:
+ *    {
+ *      "topics": [
+ *        {"id": "1", ...}
+ *        {"id": "2", ...}
+ *        ...
+ *        {"id": "n", ...}
+ *      ]
+ *    }
+ */
+exports.getTopics = function() {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'topics.json', true);
+
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        resolve(JSON.parse(this.response));
+      } else {
+        reject(this.status, this.response);
+      }
+    };
+
+    xhr.send();
+  });
+};
---
+++ app/scripts/topic-info.jsx
@@ -0,0 +1,70 @@
+/** @jsx React.DOM */
+
+var React = require('react/addons');
+
+/**
+ * Renders topic's detailed info.
+ *
+ * Usage:
+ *    <TopicInfo topic={topic} />
+ *
+ */
+var TopicInfo = React.createClass({
+  render: function() {
+    // All sentiment metrics are 0 by default
+    var sentiment = {
+      positive: 0,
+      negative: 0,
+      neutral: 0
+    };
+
+    sentiment = React.addons.update(sentiment, {
+      $merge: this.props.topic.sentiment || {}
+    });
+
+    return (
+        <article className="topic-info">
+          <header>
+            Information on topic:
+            “
+            <span className="topic-info__label">
+              {this.props.topic.label}
+            </span>
+            ”
+          </header>
+
+          <ul className="topic-info__metrics">
+            <li className="item--emphasize">
+              Total Mentions:
+              <span className="topic-info__volume">
+                {this.props.topic.volume}
+              </span>
+            </li>
+
+            <li>
+              Positive Mentions:
+              <span className="topic-info__positive">
+                {sentiment.positive}
+              </span>
+            </li>
+
+            <li>
+              Neutral Mentions:
+              <span className="topic-info__neutral">
+                {sentiment.neutral}
+              </span>
+            </li>
+
+            <li>
+              Negative Mentions:
+              <span className="topic-info__negative">
+                {sentiment.negative}
+              </span>
+            </li>
+          </ul>
+        </article>
+    );
+  }
+});
+
+module.exports = TopicInfo;
